e926e4910d938242a1b6d8dc0705f2f56da7320d
Belief <lhuangios@163.com>
2016-01-20 21:00:05 +0800

    partOne/

---
+++ partOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  partOne
+//
+//  Created by Belief on 16/1/18.
+//  Copyright © 2016年 Belief. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
